6a3be0bceb8152c5512d4482a0383c6f7d59fac8
Jakub Nitkiewicz <01158207@pw.edu.pl>
2021-06-02 22:15:23 +0200

    getting started

--- Module.h
+++ Module.h
@@ -1,9 +1,25 @@
 #pragma once
-template <typename T, size_t Lt, typename U, size_t Lu, typename V, size_t Lv>
+template <typename T, size_t Lt, typename U = int, size_t Lu = 0, typename V = int, size_t Lv = 0>
 class Module
 {
 private:
 	T arrT[Lt];
 	U arrU[Lu];
 	V arrV[Lv];
+public:
+	Module(T arrTT[Lt])
+	{
+		size_t n = 0;
+		for (auto& element : arrT)
+			element = arrTT[n++];
+	}
+	Module() {};
+	void setArguments(T arrTT[Lt], U arrUU[Lu], V arrVV[Lv])
+	{
+		arrT = arrTT; arrU = arrUU; arrV = arrVV;
+	}
+	T* getArrT()
+	{
+		return arrT;
+	}
 };
