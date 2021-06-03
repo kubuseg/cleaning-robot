707146b8d07be528d193ef640fd57ff21b3076f8
Jakub Nitkiewicz <01158207@pw.edu.pl>
2021-06-03 17:48:58 +0200

    working on a template for Modules

--- Module.h
+++ Module.h
@@ -1,25 +1,39 @@
 #pragma once
-template <typename T, size_t Lt, typename U = int, size_t Lu = 0, typename V = int, size_t Lv = 0>
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <string>
+#include <typeinfo>
+using namespace std;
+
+template <typename T, size_t lt, typename U = void, size_t lu = 0>
 class Module
 {
 private:
-	T arrT[Lt];
-	U arrU[Lu];
-	V arrV[Lv];
+	//T arrT[Lt];
+	//U arrU[Lu];
 public:
-	Module(T arrTT[Lt])
+	/*Module() {};
+	Module(T arrTT[lt])
 	{
 		size_t n = 0;
 		for (auto& element : arrT)
 			element = arrTT[n++];
+	}*/
+	if (typeid(U) != void) {
+		virtual void setArgs(const T t[lt], const U u[lu]) = 0;
+		virtual U* getArrgsU() const = 0;
+		virtual void setArgs(const U u[lu]) = 0;
 	}
-	Module() {};
-	void setArguments(T arrTT[Lt], U arrUU[Lu], V arrVV[Lv])
-	{
-		arrT = arrTT; arrU = arrUU; arrV = arrVV;
-	}
-	T* getArrT()
-	{
-		return arrT;
-	}
+	virtual void setArgs(const T t[lt]) = 0;
+	virtual T* getArrgsT() const = 0;
+
+};
+
+class RoadSet : Module<vector<pair<double, double>>, 1>
+{
+private:
+	vector<pair<double, double>> points;
+public:
+	RoadSet();
 };
